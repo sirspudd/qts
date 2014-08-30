4d63ec6d2fef996acb67991c640228acc4edeb7f
Donald Carr <sirspudd@gmail.com>
2014-08-29 23:36:34 -0700

    Hit brick wall of my JS ignorance; need internet
    Change-Id: I268823526ffd3692dddf2ef0bdba8fc4f2b6ccf9

--- qs.js
+++ qs.js
@@ -3,7 +3,10 @@
 'use strict';
 
 var program = require('commander'),
-    manifest = require('./package.json');
+    manifest = require('./package.json'),
+    fs = require('fs');
+
+var qtConfig = JSON.parse(fs.readFileSync('/Users/sirspudd/.qt-config.json'));
 
 program.version(manifest.version)
     .option('s, select [ver]', 'Select qt version [ver]')
@@ -11,6 +14,8 @@ program.version(manifest.version)
     .parse(process.argv);
 
 if (program.select) {
+   var qtVersions = fs.readdirSync(qtConfig.root);
+   console.log(qtVersions);
 
 }
 
