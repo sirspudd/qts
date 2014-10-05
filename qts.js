e1c172b2e7c2f173af5a6bc26170c3475757bb5b
Donald Carr <sirspudd@gmail.com>
2014-10-04 17:38:07 -0700

    qts WIP
    Change-Id: I2543cd8a65730016619f90d31058b738ba9bf0df

--- qts.js
+++ qts.js
@@ -4,22 +4,23 @@
 
 var program = require('commander'),
     manifest = require('./package.json'),
+    homeDir = require('home-dir'),
     fs = require('fs');
 
-var qtConfig = JSON.parse(fs.readFileSync('/Users/sirspudd/.qt-config.json'));
+var qtConfig = JSON.parse(fs.readFileSync(homeDir() + '/.qt-config.json'));
 
 program.version(manifest.version)
     .option('s, select [ver]', 'Select qt version [ver]')
     .option('l, list [ver]', 'List Qt versions [ver]')
     .parse(process.argv);
 
-if (program.select) {
-   var qtVersions = fs.readdirSync(qtConfig.root);
-   console.log(qtVersions);
-
-}
+if (program.select) {}
 
 if (program.list) {
-
+    var qtVersions = fs.readdirSync(qtConfig.root);
+    console.log(qtVersions);
+    qtVersions.forEach(function() {
+        if (str.search('^qt-(.*)'))
+    });
 }
 if (process.argv.length < 3) program.help();
